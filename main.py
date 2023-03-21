0b7888903918f43648bc1c98162227ed87e30b56
Sermob112 <motherboard879@gmail.com>
2023-03-21 21:25:28 +0300

    3 задание

--- main.py
+++ main.py
@@ -8,101 +8,292 @@ import matplotlib.lines as mlines
 from mtcnn.mtcnn import MTCNN
 import math
 import pandas as pd
+import os
+
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from hist import MainWindow
+import sys
+import os
+###########################################
+#чтение всех файлов
+file_names = []
+path = 'orl_faces'
+folders = os.listdir(path)
+etalon = []
+test = []
+i = 1
+def read_Etalon_and_test(b):
+    for folder in folders:
+        for i in range(b+1,11):
+            test.append(f'orl_faces/{folder}/{i}.pgm')
+        for j in range(1,b+1):
+            etalon.append(f'orl_faces/{folder}/{j}.pgm')
+
+
+read_Etalon_and_test(2)
+print(test)
+print(etalon)
+# for folder in folders:
+#     folder_path = os.path.join(path, folder)
+#     if os.path.isdir(folder_path):
+#         files = os.listdir(folder_path)
+#         for file in files:
+#             file_names.append(os.path.join(folder_path, file))
+# img = cv.imread(file_names[])
+# cv.imshow('Fourier Transform', img)
+# cv.waitKey(0)
+
+
+# class mywindow(QtWidgets.QMainWindow):
+#     def __init__(self):
+#         super(mywindow, self).__init__()
+#         self.ui = MainWindow()
+#         # self.ui.setupUi(self)
+#
+# def application():
+#     app = QApplication(sys.argv)
+#     window = mywindow()
+#
+#     window.show()
+#     sys.exit(app.exec_())
+# if __name__ == '__main__':
+#     application()
+
+
+# ####Гистограмма
+# img = cv.imread('s12/1.jpg')
+# # calculate mean value from RGB channels and flatten to 1D array
+# vals = img.mean(axis=2).flatten()
+# # plot histogram with 255 bins
+# b, bins, patches = plt.hist(vals, 255)
+# plt.xlim([0,255])
+# plt.show()
+# ####DFT
+#
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#
+# # Compute the discrete Fourier Transform of the image
+# fourier = cv.dft(np.float32(gray), flags=cv.DFT_COMPLEX_OUTPUT)
+#
+# # Shift the zero-frequency component to the center of the spectrum
+# fourier_shift = np.fft.fftshift(fourier)
+#
+# # calculate the magnitude of the Fourier Transform
+# magnitude = 20 * np.log(cv.magnitude(fourier_shift[:, :, 0], fourier_shift[:, :, 1]))
+#
+# # Scale the magnitude for display
+# magnitude = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX, cv.CV_8UC1)
+#
+# # Display the magnitude of the Fourier Transform
+# cv.imshow('Fourier Transform', magnitude)
+# cv.waitKey(0)
+
+###Градиент
+
+# sobelx = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=5)
+# sobely = cv.Sobel(img, cv.CV_64F, 0, 1, ksize=5)
+#
+# # Вычисление абсолютного значения градиента
+# abs_sobelx = cv.convertScaleAbs(sobelx)
+# abs_sobely = cv.convertScaleAbs(sobely)
+#
+# # Объединение градиентов по горизонтали и вертикали
+# grad = cv.addWeighted(abs_sobelx, 0.5, abs_sobely, 0.5, 0)
+#
+# # Отображение изображения с градиентом
+# cv.imshow('Gradient', grad)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+
+# ###DCT
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#
+# # Применяем DCT
+# dct = cv.dct(np.float32(gray) / 255.0)
+#
+# # Обратное DCT
+# idct = cv.idct(dct) * 255.0
+#
+# # Отображение результатов
+# cv.imshow('Original Image', img)
+# cv.imshow('DCT', dct)
+# cv.imshow('IDCT', idct)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+# imgcv1 = cv.split(img)[0]
+# imf = np.float32(imgcv1)/255.0  # float conversion/scale
+# dct = cv.dct(imf)              # the dct
+# imgcv1 = np.uint8(dct*255.0)
+#
+#
+# cv.imshow('DCT', imgcv1)
+# cv.waitKey(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def sim(filename):
+#     test = []
+#     img_rgb = cv.imread(filename)
+#     detector = MTCNN()
+#     faces = detector.detect_faces(img_rgb)
+#     data = plt.imread(filename)
+#     plt.imshow(data)
+#     for result in faces:
+#         for key, value in result['keypoints'].items():
+#             if key == 'left_eye':
+#                 test.append(value)
+#             if key == 'right_eye':
+#                 test.append(value)
+#     x, y, width, height = result['box']
+#     rect = Rectangle((x, y), width, height, fill=False, color='red')
+#     new_array = [n for tup in test for n in tup]
+#     mid_x = (new_array[0] + new_array[2]) / 2
+#     mid_y = (new_array[1] + new_array[3]) / 2
+#     dist_x = math.sqrt((new_array[0] - mid_x)**2+(new_array[1] - mid_y)**2)
+#     ax = plt.gca()
+#     # горизонтальная
+#     l = ax.axline([new_array[0], new_array[1]], [new_array[2], new_array[3]])
+#     ax.add_patch(rect)
+#     # ##основная симметричная
+#     l2 = ax.axline([x + width/2,y + height], [mid_x, mid_y])
+#     # ##дополнительная симметричная
+#     # l3 = ax.axvline(new_nose[0] + dist_x)
+#     # l4 = ax.axvline(new_nose[0] - dist_x)
+#     l3 = ax.axline([new_array[0], new_array[1]],[x + width/2- dist_x,y + height])
+#     l4 = ax.axline([new_array[2], new_array[3]], [x + width / 2 + dist_x, y + height])
+#     ###prochee
+#     # l2 = ax.axline([new_nose[0], new_nose[1]], [mid_x, mid_y])
+#     # l2 = mlines.Line2D([new_nose[0], mid_x ], [new_nose[1], mid_y])
+#     ax.add_line(l)
+#     ax.add_line(l2)
+#     ax.add_line(l3)
+#     ax.add_line(l4)
+#     plt.show()
+# sim('s12/1.jpg')
 # #############################################################################
 #template метод
-def template(temp, pic):
-    img_rgb = cv.imread(pic)
-    img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-    template = cv.imread(temp,0)
-    crop_img = template[40:95,10:80]
-    w, h = template.shape[::-1]
-    res = cv.matchTemplate(img_gray,crop_img,cv.TM_CCOEFF_NORMED)
-    threshold = 0.99
-    loc = np.where( res >= threshold)
-    for pt in zip(*loc[::-1]):
-        cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    # cv.imwrite('res.png',img_rgb)
-
-    cv.imshow("Template", crop_img)
-    cv.imshow("img_rgb", img_rgb)
-    cv.waitKey(0)
-# template('s12/1.pgm','s12/s12_all.pbm')
-#######################################################################
-
-###метод Виолы - Джонес
-def Viola_J(filename):
-
-    face_cascade = cv.CascadeClassifier('cascade/haarcascade_frontalface_default.xml')
-    eyes_cascade = cv.CascadeClassifier('cascade/haarcascade_eye.xml')
-    smile_cascade = cv.CascadeClassifier('cascade/haarcascade_smile.xml')
-    glass_cascade = cv.CascadeClassifier('cascade/haarcascade_eye_tree_eyeglasses.xml')
-    img_rgb = cv.imread(filename)
-    img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(img_gray,1.3,5)
-    eyes = eyes_cascade.detectMultiScale(img_gray,1.3,5)
-    smile = smile_cascade.detectMultiScale(img_gray,1.3,5)
-    glass = glass_cascade.detectMultiScale(img_gray,1.3,5)
-    # for (x, y, w, h) in faces:
-    #     cv.rectangle(img_rgb, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    for (ex, ey, ew, eh) in eyes:
-        cv.rectangle(img_rgb,(ex,ey),(ex+ew,ey+eh), (255,0,0),2)
-    # for (ex, ey, ew, eh) in smile:
-    #     cv.rectangle(img_rgb,(ex,ey),(ex+ew,ey+eh), (255,0,0),2)
-    # for (ex, ey, ew, eh) in glass:
-    #     cv.rectangle(img_rgb,(ex,ey),(ex+ew,ey+eh), (255,0,0),2)
-    cv.imshow('img', img_rgb)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-# Viola_J('s12/s27_all.jpg')
-##Линии симметрии
-# img_rgb = cv.imread('pic/test.jpg')
-# img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-test = []
-nose = []
-def draw_image_with_boxes(filename, result_list):
-    data = plt.imread(filename)
-    plt.imshow(data)
-    for result in result_list:
-        for key, value in result['keypoints'].items():
-            if key == 'left_eye':
-                test.append(value)
-            if key == 'right_eye':
-                test.append(value)
-    x, y, width, height = result['box']
-    rect = Rectangle((x, y), width, height, fill=False, color='red')
-    new_array = [n for tup in test for n in tup]
-    mid_x = (new_array[0] + new_array[2]) / 2
-    mid_y = (new_array[1] + new_array[3]) / 2
-    dist_x = math.sqrt((new_array[0] - mid_x)**2+(new_array[1] - mid_y)**2)
-    ax = plt.gca()
-    # горизонтальная
-    l = ax.axline([new_array[0], new_array[1]], [new_array[2], new_array[3]])
-    ax.add_patch(rect)
-    # ##основная симметричная
-    l2 = ax.axline([x + width/2,y + height], [mid_x, mid_y])
-    # ##дополнительная симметричная
-    # l3 = ax.axvline(new_nose[0] + dist_x)
-    # l4 = ax.axvline(new_nose[0] - dist_x)
-    l3 = ax.axline([new_array[0], new_array[1]],[x + width/2- dist_x,y + height])
-    l4 = ax.axline([new_array[2], new_array[3]], [x + width / 2 + dist_x, y + height])
-    ###prochee
-    # l2 = ax.axline([new_nose[0], new_nose[1]], [mid_x, mid_y])
-    # l2 = mlines.Line2D([new_nose[0], mid_x ], [new_nose[1], mid_y])
-    ax.add_line(l)
-    ax.add_line(l2)
-    ax.add_line(l3)
-    ax.add_line(l4)
-    plt.show()
-for i in range(1,8):
-    filename = f's12/{8}.pgm'
-    img_rgb = cv.imread(filename)
-    img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-    # создать детектор, используя веса по умолчанию
-    detector = MTCNN()
-    # распознать лица на изображении
-    faces = detector.detect_faces(img_rgb)
-    # отобразить лица на исходном изображении
-    draw_image_with_boxes(filename, faces)
+# def template(temp, pic):
+#     img_rgb = cv.imread(pic)
+#     img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+#     template = cv.imread(temp,0)
+#     crop_img = template[40:95,10:80]
+#     w, h = template.shape[::-1]
+#     res = cv.matchTemplate(img_gray,crop_img,cv.TM_CCOEFF_NORMED)
+#     threshold = 0.99
+#     loc = np.where( res >= threshold)
+#     for pt in zip(*loc[::-1]):
+#         cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+#     # cv.imwrite('res.png',img_rgb)
+#
+#     cv.imshow("Template", crop_img)
+#     cv.imshow("img_rgb", img_rgb)
+#     cv.waitKey(0)
+# # template('s12/1.pgm','s12/s12_all.pbm')
+# #######################################################################
+#
+# ###метод Виолы - Джонес
+# def Viola_J(filename):
+#
+#     face_cascade = cv.CascadeClassifier('cascade/haarcascade_frontalface_default.xml')
+#     eyes_cascade = cv.CascadeClassifier('cascade/haarcascade_eye.xml')
+#     smile_cascade = cv.CascadeClassifier('cascade/haarcascade_smile.xml')
+#     glass_cascade = cv.CascadeClassifier('cascade/haarcascade_eye_tree_eyeglasses.xml')
+#     img_rgb = cv.imread(filename)
+#     img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+#     faces = face_cascade.detectMultiScale(img_gray,1.3,5)
+#     eyes = eyes_cascade.detectMultiScale(img_gray,1.3,5)
+#     smile = smile_cascade.detectMultiScale(img_gray,1.3,5)
+#     glass = glass_cascade.detectMultiScale(img_gray,1.3,5)
+#     # for (x, y, w, h) in faces:
+#     #     cv.rectangle(img_rgb, (x, y), (x+w, y+h), (255, 0, 0), 2)
+#     for (ex, ey, ew, eh) in eyes:
+#         cv.rectangle(img_rgb,(ex,ey),(ex+ew,ey+eh), (255,0,0),2)
+#     # for (ex, ey, ew, eh) in smile:
+#     #     cv.rectangle(img_rgb,(ex,ey),(ex+ew,ey+eh), (255,0,0),2)
+#     # for (ex, ey, ew, eh) in glass:
+#     #     cv.rectangle(img_rgb,(ex,ey),(ex+ew,ey+eh), (255,0,0),2)
+#     cv.imshow('img', img_rgb)
+#     cv.waitKey(0)
+#     cv.destroyAllWindows()
+# # Viola_J('s12/s27_all.jpg')
+# ##Линии симметрии
+# # img_rgb = cv.imread('pic/test.jpg')
+# # img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+# test = []
+# nose = []
+# def draw_image_with_boxes(filename, result_list):
+#     data = plt.imread(filename)
+#     plt.imshow(data)
+#     for result in result_list:
+#         for key, value in result['keypoints'].items():
+#             if key == 'left_eye':
+#                 test.append(value)
+#             if key == 'right_eye':
+#                 test.append(value)
+#     x, y, width, height = result['box']
+#     rect = Rectangle((x, y), width, height, fill=False, color='red')
+#     new_array = [n for tup in test for n in tup]
+#     mid_x = (new_array[0] + new_array[2]) / 2
+#     mid_y = (new_array[1] + new_array[3]) / 2
+#     dist_x = math.sqrt((new_array[0] - mid_x)**2+(new_array[1] - mid_y)**2)
+#     ax = plt.gca()
+#     # горизонтальная
+#     l = ax.axline([new_array[0], new_array[1]], [new_array[2], new_array[3]])
+#     ax.add_patch(rect)
+#     # ##основная симметричная
+#     l2 = ax.axline([x + width/2,y + height], [mid_x, mid_y])
+#     # ##дополнительная симметричная
+#     # l3 = ax.axvline(new_nose[0] + dist_x)
+#     # l4 = ax.axvline(new_nose[0] - dist_x)
+#     l3 = ax.axline([new_array[0], new_array[1]],[x + width/2- dist_x,y + height])
+#     l4 = ax.axline([new_array[2], new_array[3]], [x + width / 2 + dist_x, y + height])
+#     ###prochee
+#     # l2 = ax.axline([new_nose[0], new_nose[1]], [mid_x, mid_y])
+#     # l2 = mlines.Line2D([new_nose[0], mid_x ], [new_nose[1], mid_y])
+#     ax.add_line(l)
+#     ax.add_line(l2)
+#     ax.add_line(l3)
+#     ax.add_line(l4)
+#     plt.show()
+# for i in range(1,8):
+#     filename = f's12/{8}.pgm'
+#     img_rgb = cv.imread(filename)
+#     img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+#     # создать детектор, используя веса по умолчанию
+#     detector = MTCNN()
+#     # распознать лица на изображении
+#     faces = detector.detect_faces(img_rgb)
+#     # отобразить лица на исходном изображении
+#     draw_image_with_boxes(filename, faces)
 
 
 ##############################################################################################################
